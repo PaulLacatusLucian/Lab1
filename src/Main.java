435c42a935046e0d15b05dc5b1bf69a3cba7836b
Paul <paul2003mai@yahoo.com>
2023-10-11 14:50:50 +0300

    Problema2Final

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-     //  ex2();
+       ex2();
        ex3();
     }
 
@@ -20,10 +20,9 @@ public class Main {
         Ex3 ex3 = new Ex3();
         int[] num1 = {1,2,3};
         int[] num2 = {1,3,4};
-        int[] num3 = {2,0,0,1};
         System.out.println(Arrays.toString(ex3.addNumbers(num1, num2)));
-//        System.out.println(Arrays.toString(ex3.subtractNumbers(num1, num2)));
-//        System.out.println(Arrays.toString(ex3.multiply(num1, 3)));
-//        System.out.println(Arrays.toString(ex3.division(num1, 3)));
+        System.out.println(Arrays.toString(ex3.subtractNumbers(num1, num2)));
+        System.out.println(Arrays.toString(ex3.multiply(num1, 3)));
+        System.out.println(Arrays.toString(ex3.division(num1, 3)));
     }
 }
